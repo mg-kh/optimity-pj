31e14dc25a0eaedecafb6069fdfeccc87a4fb0d1
mg-kh <tragonn18@gmail.com>
2020-10-08 17:17:14 +0630

    init

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+import $ from "jquery";
+import "bootstrap";
+// import Swiper from "swiper";
+// import "swiper/swiper-bundle.css";
+
+import "../scss/style.scss";
+
+$(document).ready(function () {
+  //..
+});
