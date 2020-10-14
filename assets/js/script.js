e0270b059af6ca4fb05b3c0d205e41a9742d4738
mg-kh <tragonn18@gmail.com>
2020-10-14 20:10:55 +0630

    wp_nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,32 @@ import * as $ from "jquery";
 import "bootstrap";
 
 $(document).ready(function() {
-  //..
+  // console.log(text);
+  $(".sub-menu").addClass("animate__animated animate__fadeIn");
+  $(".side__menu").addClass("animate__animated animate__fadeInRight");
+
+  $(".show__side__menu").click(function() {
+    if (
+      $(this)
+        .children()
+        .hasClass("fa-bars")
+    ) {
+      $(this)
+        .children()
+        .removeClass("fa-bars");
+      $(this)
+        .children()
+        .addClass("fa-times");
+    } else {
+      $(this)
+        .children()
+        .removeClass("fa-times");
+      $(this)
+        .children()
+        .addClass("fa-bars");
+    }
+    // console.log($(this).children());
+    // <i class="fas fa-times"></i>
+    $(".side__menu").toggleClass("d-none");
+  });
 });
